6f8c3de6233c28d00fa8223f58fcd29aaad81209
JabalThoriq16 <jabal.thoriq16@mhs.uinjkt.ac.id>
2025-06-04 21:16:39 +0700

    update swagger

--- pages/api/docs.js
+++ pages/api/docs.js
@@ -1,5 +1,24 @@
-import swaggerDocument from '../../swagger.json';
+// pages/api-docs.js
+import dynamic from 'next/dynamic';
+import Head from 'next/head';
 
-export default function handler(req, res) {
-  res.status(200).json(swaggerDocument);
+// SwaggerUI hanya bisa dijalankan di client-side
+const SwaggerUI = dynamic(
+  () => import('swagger-ui-react').then((mod) => mod.default),
+  { ssr: false }
+);
+
+export default function ApiDocs() {
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/swagger-ui-dist@4/swagger-ui.css"
+        />
+        <title>Dokumentasi API</title>
+      </Head>
+      <SwaggerUI url="/openapi.json" />
+    </>
+  );
 }
